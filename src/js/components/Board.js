2a87c567c35cf9eb3109a8eff72d4b8bbe424c19
Tomasz Ferens <tomaszferens@hotmail.com>
2017-01-23 18:27:52 +0100

    refactored by mapping board

--- src/js/components/Board.js
+++ src/js/components/Board.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap';
+import dataPoints from '../data.js';
 
 function Board(props) {
     function handlePlayer(i, points, length) {
@@ -9,13 +9,14 @@ function Board(props) {
             props.addPoint(i, points);
             if (props.players[i].throws === 2) {
                 props.clearThrows(i);
-                props.changePlayer(props.players[(i+1) % length].name);
+                props.changePlayer(props.players[(i + 1) % length].name);
             }
         } else if (props.players[i].score - points < 0) {
             props.incrementThrows(i);
+            props.addPoint(i, points);
             if (props.players[i].throws === 2) {
                 props.clearThrows(i);
-                props.changePlayer(props.players[(i+1) % length].name);
+                props.changePlayer(props.players[(i + 1) % length].name);
             }
         } else {
             props.changeScore(i, points);
@@ -53,6 +54,7 @@ function Board(props) {
         alignmentBaseline: 'middle'
     };
 
+
     return (
         <svg style={props.isStarted ? animation : null} version="1.1" viewBox="-250 -250 500 500" width="200mm">
             <defs>
@@ -66,150 +68,28 @@ function Board(props) {
             <g transform="scale(1,-1)" >
                 <circle fill="black" r="226" data-point="0" onClick={(e) => handleClick(e)} />
                 <g id="dartboard">
-                    <g>
-                        <use fill="red" xlinkHref="#double" data-point="40" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="20" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="60" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="20" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(18)">
-                        <use fill="green" xlinkHref="#double" data-point="10" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="5" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="15" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="5" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(36)">
-                        <use fill="red" xlinkHref="#double" data-point="24" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="12" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="36" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="12" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(54)">
-                        <use fill="green" xlinkHref="#double" data-point="18" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="9" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="27" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="9" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(72)">
-                        <use fill="red" xlinkHref="#double" data-point="28" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="14" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="42" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="14" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(90)">
-                        <use fill="green" xlinkHref="#double" data-point="22" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="11" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="33" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="11" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(108)">
-                        <use fill="red" xlinkHref="#double" data-point="16" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="8" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="24" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="8" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(126)">
-                        <use fill="green" xlinkHref="#double" data-point="32" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="16" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="48" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="16" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(144)">
-                        <use fill="red" xlinkHref="#double" data-point="14" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="7" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="21" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="7" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(162)">
-                        <use fill="green" xlinkHref="#double" data-point="38" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="19" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="57" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="19" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(180)">
-                        <use fill="red" xlinkHref="#double" data-point="6" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="3" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="9" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="3" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(198)">
-                        <use fill="green" xlinkHref="#double" data-point="34" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="17" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="51" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="17" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(216)">
-                        <use fill="red" xlinkHref="#double" data-point="4" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="2" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="6" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="2" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(234)">
-                        <use fill="green" xlinkHref="#double" data-point="30" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="15" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="45" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="15" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(252)">
-                        <use fill="red" xlinkHref="#double" data-point="20" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="10" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="30" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="10" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(270)">
-                        <use fill="green" xlinkHref="#double" data-point="12" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="6" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="18" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="6" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(288)">
-                        <use fill="red" xlinkHref="#double" data-point="26" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="13" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="39" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="13" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(306)">
-                        <use fill="green" xlinkHref="#double" data-point="8" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="4" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="12" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="4" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(324)">
-                        <use fill="red" xlinkHref="#double" data-point="36" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#outer" data-point="18" onClick={(e) => handleClick(e)} />
-                        <use fill="red" xlinkHref="#triple" data-point="54" onClick={(e) => handleClick(e)} />
-                        <use fill="black" xlinkHref="#inner" data-point="18" onClick={(e) => handleClick(e)} />
-                    </g>
-                    <g transform="rotate(342)">
-                        <use fill="green" xlinkHref="#double" data-point="2" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#outer" data-point="1" onClick={(e) => handleClick(e)} />
-                        <use fill="green" xlinkHref="#triple" data-point="3" onClick={(e) => handleClick(e)} />
-                        <use fill="white" xlinkHref="#inner" data-point="1" onClick={(e) => handleClick(e)} />
-                    </g>
+                    {
+                        dataPoints.map((item, key) => (
+                            <g transform={`rotate(${item.rotate})`} key={key}>
+                                <use fill={item.firstFill} xlinkHref="#double" data-point={`${item.double}`} onClick={(e) => handleClick(e)} />
+                                <use fill={item.secondFill} xlinkHref="#outer" data-point={`${item.outer}`}  onClick={(e) => handleClick(e)} />
+                                <use fill={item.firstFill} xlinkHref="#triple" data-point={`${item.triple}`}  onClick={(e) => handleClick(e)} />
+                                <use fill={item.secondFill} xlinkHref="#inner" data-point={`${item.inner}`}  onClick={(e) => handleClick(e)} />
+                            </g>
+                        ))
+                    }
+
                     <circle fill="green" r="16.4" strokeWidth="0" data-point="25" onClick={(e) => handleClick(e)} />
                     <circle fill="red" r="6.85" strokeWidth="0" data-point="50" onClick={(e) => handleClick(e)} />
                     <g id="grid">
                         <use xlinkHref="#refwire" />
-                        <use transform="rotate(18)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(36)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(54)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(72)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(90)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(108)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(126)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(144)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(162)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(180)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(198)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(216)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(234)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(252)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(270)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(288)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(306)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(324)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-                        <use transform="rotate(342)" xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
-
+                        {
+                            dataPoints
+                                .filter(item => item.rotate > 0)
+                                .map(item => (
+                                    <use transform={`rotate(${item.rotate})`} xlinkHref="#refwire" data-point="0" onClick={(e) => handleClick(e)} />
+                                ))
+                        }
                         <circle fill="none" r="169.5" stroke="Silver" strokeWidth="1" data-point="0" onClick={(e) => handleClick(e)} />
                         <circle fill="none" r="160.5" stroke="Silver" strokeWidth="1" data-point="0" onClick={(e) => handleClick(e)} />
                         <circle fill="none" r="106.5" stroke="Silver" strokeWidth="1" data-point="0" onClick={(e) => handleClick(e)} />
---
+++ src/js/data.js
@@ -0,0 +1,187 @@
+const dataPoints = [
+    {
+        rotate: 0,
+        double: 40,
+        outer: 20,
+        triple: 60,
+        inner: 20,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 18,
+        double: 10,
+        outer: 5,
+        triple: 15,
+        inner: 5,
+        firstFill: 'green',
+        secondFill: 'white'
+
+    },
+    {
+        rotate: 36,
+        double: 24,
+        outer: 12,
+        triple: 36,
+        inner: 12,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 54,
+        double: 18,
+        outer: 9,
+        triple: 27,
+        inner: 9,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 72,
+        double: 28,
+        outer: 14,
+        triple: 42,
+        inner: 14,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 90,
+        double: 22,
+        outer: 11,
+        triple: 33,
+        inner: 11,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 108,
+        double: 16,
+        outer: 8,
+        triple: 24,
+        inner: 8,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 126,
+        double: 32,
+        outer: 16,
+        triple: 48,
+        inner: 16,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 144,
+        double: 14,
+        outer: 7,
+        triple: 21,
+        inner: 7,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 162,
+        double: 38,
+        outer: 19,
+        triple: 57,
+        inner: 19,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 180,
+        double: 6,
+        outer: 3,
+        triple: 9,
+        inner: 3,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 198,
+        double: 34,
+        outer: 17,
+        triple: 51,
+        inner: 17,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 216,
+        double: 4,
+        outer: 2,
+        triple: 6,
+        inner: 2,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 234,
+        double: 30,
+        outer: 15,
+        triple: 45,
+        inner: 15,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 252,
+        double: 20,
+        outer: 10,
+        triple: 30,
+        inner: 10,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 270,
+        double: 12,
+        outer: 6,
+        triple: 18,
+        inner: 6,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 288,
+        double: 26,
+        outer: 13,
+        triple: 39,
+        inner: 13,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 306,
+        double: 8,
+        outer: 4,
+        triple: 12,
+        inner: 4,
+        firstFill: 'green',
+        secondFill: 'white'
+    },
+    {
+        rotate: 324,
+        double: 36,
+        outer: 18,
+        triple: 54,
+        inner: 18,
+        firstFill: 'red',
+        secondFill: 'black'
+    },
+    {
+        rotate: 342,
+        double: 2,
+        outer: 1,
+        triple: 3,
+        inner: 1,
+        firstFill: 'green',
+        secondFill: 'white'
+    }
+];
+
+export default dataPoints;
+
+
